c3da0503ce1b593d300c7cc717a07b35941f13f0
Филип Арсовски <arshetamine@protonmail.com>
2021-04-04 07:07:13 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,67 @@
+# Cook Book
+
+#### _Task 1_ ❌
+
+```
+Using the cookbook.js file:
+Use the createCard method to draw all recipes on screen.
+Hint: it’s simplest to iterate over all recipes and invoke createCard for each of them.
+```
+
+#### _Task 2_ ❌
+
+```
+Create a “Router”
+Create logic into handleRoute function
+If the hash is empty, render the “home” screen
+(cardContainerelement, use display = ‘flex’, otherwise display=‘none’)
+Besure that reloading the page should keep the
+last shown container visible
+```
+
+#### _Task 3_ ❌
+
+```
+Clicking on the “Open Recipe” button should show recipe details
+Extend the router, hide the cardContainer and show fullRecipeContainer
+section Invoke the renderRecipe function
+```
+
+#### _Task 4_ ❌
+
+```
+The tags in the recipe list aren’t currently clickable - let’s fix that 😊
+Add a ‘click’ event listener to every tag element (see comments), which
+simply logs the tag text;
+Let’s first filter the data we need - filter the recipeData variable
+```
+
+#### _Task 5_ ❌
+
+```
+Extend router functionality
+On click on tag hash should change to #tag-tagname,
+handle this in router, hide cardContainer and fullRecipeContainer,
+but set tagContainer to display = “flex”.
+Filtered data from the previous step should now
+be displayed into tagContainer
+Use createCard function for each of the filtered recipes
+```
+
+#### _Task 6_ ❌
+
+```
+Add a click event listener on the logo (header element), that should bring us on the homepage (empty hash).
+```
+
+#### _Task 7_ ❌
+
+```
+On the homepage under “What are we cooking today?” add a simple search form with search button.
+
+Clicking on the button should set new hash ‘#search-SEARCHED-VALUE’
+FilterrecipeData only by the name.
+
+Filtered data should be displayed in new container
+(Feel free to add new section).
+```
---
+++ js/cookbook.js
@@ -0,0 +1,99 @@
+let cardContainer = document.querySelector('#card-container');
+let fullRecipeContainer = document.querySelector('#full-recipe-container');
+let searchContainer = document.querySelector('#search-container');
+let tagContainer = document.querySelector('#tag-container');
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function createCard(cardData) {
+  let card = document.createElement('div');
+  let cimage = document.createElement('div');
+  let title = document.createElement('h5');
+  let contents = document.createElement('div');
+  let actions = document.createElement('div');
+
+  card.classList.add('card');
+
+  cimage.classList.add('card-image');
+  let img = document.createElement('img');
+  img.src = './images/' + getRandomInt(1, 29) + '.jpg';
+  cimage.appendChild(img);
+
+  title.innerText = cardData.name;
+
+  contents.classList.add('card-content');
+  contents.appendChild(title);
+
+  //Here make changes on tags
+  cardData.tags.forEach((tag) => {
+    let tagLink = document.createElement('a');
+    tagLink.innerText = tag
+    contents.appendChild(tagLink);
+  });
+
+  actions.classList.add('card-action');
+  let link = document.createElement('a');
+  link.innerText = "Open Recipe";
+  link.classList.add('waves-effect', 'waves-light', 'btn', 'orange');
+  link.href = '#' + cardData.id;
+  actions.appendChild(link);
+
+  card.appendChild(cimage);
+  card.appendChild(contents);
+  card.appendChild(actions);
+
+  return card;
+};
+
+function renderRecipe() {
+  let id = location.hash.replace('#', '');
+  let recipe = recipeData.find(r => r.id === id);
+  fullRecipeContainer.innerHTML = '';
+  
+  let wrapper = document.createElement('div');
+  let card = document.createElement('div');
+  let cimage = document.createElement('div');
+  let title = document.createElement('h3');
+  let contents = document.createElement('div');
+
+  wrapper.classList.add('full-recipe-wrapper');
+
+  title.innerText = recipe.name;
+
+  cimage.classList.add('card-image');
+  let img = document.createElement('img');
+  img.src = './images/' + getRandomInt(1, 29) + '.jpg';
+  cimage.appendChild(img);
+  
+  contents.classList.add('card-content');
+  let p = document.createElement('p');
+  p.innerText = recipe.instructions;
+  let h4Instructions = document.createElement('h4');
+  h4Instructions.innerText = 'Instructions';
+
+  contents.appendChild(h4Instructions);
+  contents.appendChild(p);
+
+  card.classList.add('card');
+  card.appendChild(cimage);
+  card.appendChild(contents);
+
+  wrapper.appendChild(title);
+  wrapper.appendChild(card);
+
+  fullRecipeContainer.appendChild(wrapper);
+  
+}
+
+//Exercise 6 - return to home page
+
+
+// Exercise 1 - draw cards on screen using the
+// createCard function.
+
+
+//Exercise 2
